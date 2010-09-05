1427ad6fdbde2fb42e8807fd45894ebfd29e6f10
yinqiwen <yinqiwen@d5262f74-ff58-11de-89cc-8daf83ecd611>
2010-09-05 04:27:07 +0000

    Remove for V0.9.0

--- shell/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set HYK_PROXY_CLIENT_HOME=%~dp0\..
-@java -cp "%~dp0\..\dist\hyk-proxy-client.jar;%~dp0\..\etc" "-DHYK_PROXY_CLIENT_HOME=%HYK_PROXY_CLIENT_HOME%" com.hyk.proxy.client.launch.tui.StartProxyLocalServer %*
--- src/com/hyk/proxy/common/Version.java
+++
@@ -1,1 +0,0 @@
-package com.hyk.proxy.common;    public class Version    {       public static final String value = "0.9.0beta";    }
