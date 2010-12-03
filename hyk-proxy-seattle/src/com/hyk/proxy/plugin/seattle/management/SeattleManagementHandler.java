c61c9ec11db2a19cb7cb2d4f7e36f60babe35461
yinqiwen <yinqiwen@d5262f74-ff58-11de-89cc-8daf83ecd611>
2010-12-03 09:30:32 +0000

    Bug fix & new feature

--- hyk-proxy-seattle/src/com/hyk/proxy/plugin/seattle/management/SeattleManagementHandler.java
+++ hyk-proxy-seattle/src/com/hyk/proxy/plugin/seattle/management/SeattleManagementHandler.java
@@ -3,16 +3,31 @@
  */
 package com.hyk.proxy.plugin.seattle.management;
 
+import java.io.Console;
+import java.net.Socket;
+import java.util.List;
+
+import com.hyk.proxy.framework.common.Version;
 import com.hyk.proxy.framework.management.ManageResource;
 import com.hyk.proxy.framework.plugin.PluginAdmin;
+import com.hyk.proxy.framework.util.SimpleSocketAddress;
+import com.hyk.proxy.plugin.seattle.config.SeattleApplicationConfig;
 import com.hyk.proxy.plugin.seattle.event.SeattleProxyEventServiceFactory;
 
 /**
  * @author qiyingwang
- *
+ * 
  */
 public class SeattleManagementHandler implements ManageResource, PluginAdmin
 {
+
+	public static void output(String msg)
+	{
+		if (null != msg)
+		{
+			System.out.print(msg);
+		}
+	}
 
 	@Override
 	public String handleManagementCommand(String cmd)
@@ -25,11 +40,60 @@ public class SeattleManagementHandler implements ManageResource, PluginAdmin
 	{
 		return SeattleProxyEventServiceFactory.NAME;
 	}
+	
+	private void showServerStatus()
+	{
+		List<SimpleSocketAddress> addrs = SeattleApplicationConfig.getSeattleServerAddress();
+		for(SimpleSocketAddress addr:addrs)
+		{
+			output(addr.toString());
+			output("           ");
+			try
+			{
+				long start = System.currentTimeMillis();
+				Socket s = new Socket(addr.host, addr.port);
+				s.close();
+				long end = System.currentTimeMillis();
+				output((end-start) + "ms");
+			}
+			catch (Exception e)
+			{
+				output("Not available");
+			}
+			System.out.println();
+		}
+	}
 
 	@Override
 	public void start()
 	{
-				
+		Console console = System.console();
+		while (true)
+		{
+			System.out.println("==============SeattleGENI Admin=============");
+			System.out.println("[1] List Remote Servers");
+			System.out.println("[0] Return");
+			System.out.print("Please enter 0-1:");
+			String s = console.readLine();
+			try
+			{
+				int choice = Integer.parseInt(s);
+				if (0 == choice)
+				{
+					break;
+				}
+				else if (1 == choice)
+				{
+					showServerStatus();
+					continue;
+				}
+			}
+			catch (Exception e)
+			{
+				// TODO: handle exception
+			}
+			System.err.println("Wrong input:" + s);
+		}
 	}
 
 }
