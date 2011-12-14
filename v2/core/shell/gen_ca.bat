ec81a12251e3debc9081bf25929438e784984c92
yinqiwen <yinqiwen@d5262f74-ff58-11de-89cc-8daf83ecd611>
2011-12-14 11:42:04 +0000

    Project rename&move to http://snova.googlecode.com

--- v2/core/shell/gen_ca.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set HYK_PROXY_HOME=%~dp0\..
-@java -cp "%HOME%\.hyk-proxy\.update\lib\hyk-proxy-launch.jar;%HOME%\.hyk-proxy\.update\etc;%~dp0\..\lib\hyk-proxy-launch.jar;%~dp0\..\etc" "-DHYK_PROXY_HOME=%HYK_PROXY_HOME%" com.hyk.proxy.framework.util.SslCertificateHelper
